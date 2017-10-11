509bf178d743cf7c53ebd636472d3a72121ddb28
Abdullah <AbdullahNoman@users.noreply.github.com>
2017-10-11 23:58:53 +0100

    mux2to1

--- anoman/lab1/src/mux2to1.vhd
+++ anoman/lab1/src/mux2to1.vhd
@@ -1,17 +1,17 @@
 --Engineer: Abdullah Noman
---Description: mux_2_to_1
+--Description: mux_2to1
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity mux_2_to_1 is
+entity mux_2to1 is
 	Port (i0: in std_logic);
 		 (i1: in std_logic);
 		 (s0: in std_logic);
 		 (f: out std_logic);
-end mux_2_to_1;
+end mux_2to1;
 
-architecture my_architecture of mux_2_to_1 is
+architecture my_architecture of mux_2to1 is
 begin
    process(io,i1,so)
    begin
