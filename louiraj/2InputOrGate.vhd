7ffd3b7833fb6067a000621ee005b52d1e4c5e89
louiraj <lo2ayr@gmail.com>
2017-10-11 21:17:35 +0100

    2InputOrGatetest
    2InputOrGatetest

--- louiraj/2InputOrGate.vhd
+++ louiraj/2InputOrGate.vhd
@@ -2,15 +2,15 @@
 -- 2input or Gate
 
 library ieee;
-use ieee.std_logic_1164.all
+use ieee.std_logic_1164.all;
 
-entity orgate2 is Port(
-A : in std_logic;
-B : in std_logic;
-Y : out std_logic
-);
-end orgate2;
-architecture func of orgate2 is
+entity orGate2 is port(
+   A : in std_logic;
+   B : in std_logic;
+   F : out std_logic);
+end orGate2;
+
+architecture func of orGate2 is
 begin
-F <= A or B; after ns 5;
+   F <= A or B;
 end func;
