8fc49d4e0c23d42448f9464fb4a7833f99787a03
Abdullah <AbdullahNoman@users.noreply.github.com>
2017-10-24 16:45:52 +0100

    Shift Control Logic

---
+++ anoman/lab2/src/shift_control_logic.vhd
@@ -0,0 +1,20 @@
+ --------------------------------------------------------------------------------
+ 
+-- Engineer: Abdullah Noman
+-- Description:	 Shift Control Logic
+
+--------------------------------------------------------------------------------
+
+library IEEE;  
+use IEEE.STD_LOGIC_1164.ALL;  
+
+entity shift_control_logic is  
+	Port ( input : in STD_LOGIC_VECTOR (2 downto 0);  
+		output : out STD_LOGIC_VECTOR (1 downto 0));  
+	end shift_control_logic;  
+
+architecture Behavioral of shift_control_logic is  
+begin  
+	output(0)<= (not input(2)) and (input(1) or input(0));  
+	output(1)<= ( input(2)) and (input(1) or input(0));  
+end Behavioral;  
