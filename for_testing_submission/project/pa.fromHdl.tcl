f6208637b4c8d9beb3b46f7b76f5a42ab12b11a5
Abdullah <AbdullahNoman@users.noreply.github.com>
2017-11-22 17:26:49 +0000

    lab4 tests

--- for_testing_submission/project/pa.fromHdl.tcl
+++ for_testing_submission/project/pa.fromHdl.tcl
@@ -1,64 +1,22 @@
 
 # PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
 
-create_project -name microprogram_cdp_test_VHDL -dir "C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/planAhead_run_1" -part xc3s500eft256-4
+create_project -name microprogram_cdp_test_VHDL -dir "E:/for_testing_submission/project/planAhead_run_3" -part xc3s500eft256-4
 set_param project.pinAheadLayout yes
 set srcset [get_property srcset [current_run -impl]]
-set_property target_constrs_file "C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/microprogram_cdp_test_VHDL/four_bit_alu.ucf" [current_fileset -constrset]
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/OR.vhd}]]
+set_property target_constrs_file "E:/for_testing_submission/project/microprogram_cdp_test_VHDL/four_bit_synchronous_counter.ucf" [current_fileset -constrset]
+set hdlfile [add_files [list {microprogram_cdp_test_VHDL/d_flipflop.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/half_adder.vhd}]]
+set hdlfile [add_files [list {../src/lab4/next_state_logic.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/full_adder.vhd}]]
+set hdlfile [add_files [list {microprogram_cdp_test_VHDL/nbit_reg.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/XOR.vhd}]]
+set hdlfile [add_files [list {../src/lab4/four_bit_synchronous_counter.vhd}]]
 set_property file_type VHDL $hdlfile
 set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/NOT.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/nbit_adder.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/four_bit_LAC.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/AND.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/two_input_multiplexer.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/nbit_xor_contol.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/four_input_mux.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/four_bit_LAC_adder.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/one_bit_logic_slice.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/nbit_two_input_mux.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/four_bit_adder_subtractor.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/nbit_logic_unit.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/four_bit_arithmetic_unit.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set hdlfile [add_files [list {microprogram_cdp_test_VHDL/four_bit_alu.vhd}]]
-set_property file_type VHDL $hdlfile
-set_property library work $hdlfile
-set_property top four_bit_alu $srcset
-add_files [list {C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/microprogram_cdp_test_VHDL/four_bit_alu.ucf}] -fileset [get_property constrset [current_run]]
+set_property top four_bit_synchronous_counter $srcset
+add_files [list {E:/for_testing_submission/project/microprogram_cdp_test_VHDL/four_bit_synchronous_counter.ucf}] -fileset [get_property constrset [current_run]]
 open_rtl_design -part xc3s500eft256-4
--- for_testing_submission/project/pa.fromNetlist.tcl
+++ for_testing_submission/project/pa.fromNetlist.tcl
@@ -1,11 +1,11 @@
 
 # PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
 
-create_project -name microprogram_cdp_test_VHDL -dir "C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/planAhead_run_2" -part xc3s500eft256-4
+create_project -name microprogram_cdp_test_VHDL -dir "E:/for_testing_submission/project/planAhead_run_3" -part xc3s500eft256-4
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/four_bit_alu.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project} }
+set_property edif_top_file "E:/for_testing_submission/project/modm_async_count.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {E:/for_testing_submission/project} }
 set_param project.pinAheadLayout  yes
-set_property target_constrs_file "C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/microprogram_cdp_test_VHDL/four_bit_alu.ucf" [current_fileset -constrset]
-add_files [list {C:/Users/neoke/Desktop/DSD-E/for_testing_submission/project/microprogram_cdp_test_VHDL/four_bit_alu.ucf}] -fileset [get_property constrset [current_run]]
+set_property target_constrs_file "E:/for_testing_submission/project/microprogram_cdp_test_VHDL/modm_async_count.ucf" [current_fileset -constrset]
+add_files [list {E:/for_testing_submission/project/microprogram_cdp_test_VHDL/modm_async_count.ucf}] -fileset [get_property constrset [current_run]]
 link_design
